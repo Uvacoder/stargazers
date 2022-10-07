ab1fe6d3ee5072bbb13af113bb2fd3f0418ad16b
Uvacoder <uva22940coder@gmail.com>
2022-10-06 22:49:12 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel';
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
   output: 'server',
+  adapter: vercel(),
 });
